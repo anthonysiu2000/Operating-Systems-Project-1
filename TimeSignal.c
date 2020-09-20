65ecf0e24c5bd392254eeb018577340c01968765
anthonysiu2000 <tonysiu123@yahoo.com>
2020-09-20 16:17:21 -0400

    implemented syscall

--- TimeSignal.c
+++ TimeSignal.c
@@ -13,25 +13,7 @@ void handle_sigfpe(int signum){
 
 int main(int argc, char *argv[]){
 
-	//declares structures that store seconds and microseconds
-	struct timeval start, end;
-	gettimeofday(&start, NULL);
 
-	for (int i = 0; i < 100000; i++) {
-		getpid();
-	}
-
-	gettimeofday(&end, NULL);
-	//obtains time elapsed in microseconds
-	long seconds = end.tv_sec - start.tv_sec;
-	long microseconds = end.tv_usec - start.tv_usec;
-	long microElapsed = seconds * 1000000 + microseconds;
-	long averageTime  = microElapsed / 100000;
-
-	//prints output
-	printf("Syscalls Performed: 100000\n");
-	printf("Total Elapsed Time: %d microseconds\n", microElapsed);
-	printf("Average Time Per Syscall: %d microseconds\n", averageTime);
 
 	return 0;
 
--- TimeSysCall.c
+++ TimeSysCall.c
@@ -5,12 +5,28 @@
 
 int main(int argc, char *argv[]){
 
-	/*
-	 *
-	 * Implement Here
-	 *
-	 *
-	 */
+		//declares structures that store seconds and microseconds
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
+
+		//loops through the system call 100000 times
+		for (int i = 0; i < 100000; i++) {
+			getpid();
+		}
+
+		gettimeofday(&end, NULL);
+		//obtains time elapsed in microseconds
+		long seconds = end.tv_sec - start.tv_sec;
+		long microseconds = end.tv_usec - start.tv_usec;
+		long microElapsed = seconds * 1000000 + microseconds;
+		long averageTime  = microElapsed / 100000;
+
+		//prints output
+		printf("Syscalls Performed: 100000\n");
+		printf("Total Elapsed Time: %d microseconds\n", microElapsed);
+		printf("Average Time Per Syscall: %d microseconds\n", averageTime);
+
+		return 0;
 
 	return 0;
 
