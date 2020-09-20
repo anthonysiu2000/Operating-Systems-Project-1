d027c227fc30eb3cbeb96b89113abd41f6e399b8
anthonysiu2000 <tonysiu123@yahoo.com>
2020-09-20 16:28:23 -0400

    Final SysCall Implementation

--- TimeSysCall.c
+++ TimeSysCall.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-// Place any necessary global variables here
 
 int main(int argc, char *argv[]){
 
@@ -16,18 +18,16 @@ int main(int argc, char *argv[]){
 
 		gettimeofday(&end, NULL);
 		//obtains time elapsed in microseconds
-		long seconds = end.tv_sec - start.tv_sec;
-		long microseconds = end.tv_usec - start.tv_usec;
-		long microElapsed = seconds * 1000000 + microseconds;
-		long averageTime  = microElapsed / 100000;
+		float seconds = end.tv_sec - start.tv_sec;
+		float microseconds = end.tv_usec - start.tv_usec;
+		float microElapsed = seconds * 1000000 + microseconds;
+		float averageTime = microElapsed / 100000;
 
 		//prints output
 		printf("Syscalls Performed: 100000\n");
-		printf("Total Elapsed Time: %d microseconds\n", microElapsed);
-		printf("Average Time Per Syscall: %d microseconds\n", averageTime);
+		printf("Total Elapsed Time: %f microseconds\n", microElapsed);
+		printf("Average Time Per Syscall: %f microseconds\n", averageTime);
 
 		return 0;
-
-	return 0;
 
 }
