d70382c55176a239a91f662c00112998353007f5
anthonysiu2000 <tonysiu123@yahoo.com>
2020-09-20 16:36:53 -0400

    output code added

--- TimeFork.c
+++ TimeFork.c
@@ -1,9 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-// Place any necessary global variables here
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]){
+
+	//declares structures that store seconds and microseconds
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+
+
+
+	gettimeofday(&end, NULL);
+	//obtains time elapsed in microseconds
+	float seconds = end.tv_sec - start.tv_sec;
+	float microseconds = end.tv_usec - start.tv_usec;
+	float microElapsed = seconds * 1000000 + microseconds;
+	float averageTime = microElapsed / 100000;
 
 	//prints output
 	printf("Forks Performed: 5000\n");
--- TimeSignal.c
+++ TimeSignal.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 void handle_sigfpe(int signum){
 
@@ -9,6 +11,19 @@ void handle_sigfpe(int signum){
 }
 
 int main(int argc, char *argv[]){
+
+	//declares structures that store seconds and microseconds
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+
+
+
+	gettimeofday(&end, NULL);
+	//obtains time elapsed in microseconds
+	float seconds = end.tv_sec - start.tv_sec;
+	float microseconds = end.tv_usec - start.tv_usec;
+	float microElapsed = seconds * 1000000 + microseconds;
+	float averageTime = microElapsed / 100000;
 
 	//prints output
 	printf("Exceptions Occurred: \n");
