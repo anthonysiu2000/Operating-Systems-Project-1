c1622c67a86242d45d3c698d1f9c2b134f97c4c6
anthonysiu2000 <tonysiu123@yahoo.com>
2020-09-20 17:12:09 -0400

    Final Fork Implementation

--- TimeFork.c
+++ TimeFork.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
@@ -10,14 +11,22 @@ int main(int argc, char *argv[]){
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
 
-
+	//forks the process 5000 times
+	for (int i = 0; i < 5000; i++) {
+		pid_t pid = fork();
+		if (pid == 0) {
+			exit(0);
+		} else {
+			wait(NULL);
+		}
+	}
 
 	gettimeofday(&end, NULL);
 	//obtains time elapsed in microseconds
 	float seconds = end.tv_sec - start.tv_sec;
 	float microseconds = end.tv_usec - start.tv_usec;
 	float microElapsed = seconds * 1000000 + microseconds;
-	float averageTime = microElapsed / 100000;
+	float averageTime = microElapsed / 5000;
 
 	//prints output
 	printf("Forks Performed: 5000\n");
