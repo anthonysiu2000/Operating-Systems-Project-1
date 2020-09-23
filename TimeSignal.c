f45341b3186c8cccaa69997852992e1ef4102103
anthonysiu2000 <tonysiu123@yahoo.com>
2020-09-23 14:33:38 -0400

    Final TimeSignal Implementation

--- TimeSignal.c
+++ TimeSignal.c
@@ -3,33 +3,45 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+
+int globalSignalCount = 0;
+//declares structures that store seconds and microseconds
+struct timeval start, end;
 
 void handle_sigfpe(int signum){
+	if (globalSignalCount < 100000) {
+		globalSignalCount++;
+		return;
+	}
 
-	// Handler code goes here
+		gettimeofday(&end, NULL);
+		//obtains time elapsed in microseconds
+		float seconds = end.tv_sec - start.tv_sec;
+		float microseconds = end.tv_usec - start.tv_usec;
+		float microElapsed = seconds * 1000000 + microseconds;
+		float averageTime = microElapsed / 100000;
 
+		//prints output
+		printf("Exceptions Occurred: 100000\n");
+		printf("Total Elapsed Time: %f microseconds\n", microElapsed);
+		printf("Average Time Per Exception: %f microseconds\n", averageTime);
+		exit(0);
 }
+
+
 
 int main(int argc, char *argv[]){
 
-	//declares structures that store seconds and microseconds
-	struct timeval start, end;
 	gettimeofday(&start, NULL);
 
+	//divides by zero 100000 times
+	int a = 1;
+	int b = 0;
+	int c = 0;
+	signal(SIGFPE, handle_sigfpe);
+	c = a / b;
 
-
-	gettimeofday(&end, NULL);
-	//obtains time elapsed in microseconds
-	float seconds = end.tv_sec - start.tv_sec;
-	float microseconds = end.tv_usec - start.tv_usec;
-	float microElapsed = seconds * 1000000 + microseconds;
-	float averageTime = microElapsed / 1;
-
-	//prints output
-	printf("Exceptions Occurred: \n");
-	printf("Total Elapsed Time: %f microseconds\n", microElapsed);
-	printf("Average Time Per Exception: %f microseconds\n", averageTime);
-
-	return 0;
+	return c;
 
 }
